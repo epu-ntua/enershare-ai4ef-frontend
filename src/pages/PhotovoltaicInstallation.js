4f8fea63b5d886ea2b2311d3671a0c5f0155ffe8
gkorbakis <gkorbakis@epu.ntua.gr>
2023-11-09 16:11:01 +0200

    PhotovoltaicInstallation.js: Card section implementation / images updates

--- src/pages/PhotovoltaicInstallation.js
+++ src/pages/PhotovoltaicInstallation.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useTheme } from '@mui/material/styles';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import {useTheme} from '@mui/material/styles';
 
 import {
     Container,
@@ -14,14 +14,56 @@ import {
     FormControl,
     InputLabel,
     Select,
-    MenuItem,
+    MenuItem, Card, Box, IconButton,
 } from '@mui/material';
 
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
-import Breadcrumb from '../components/layout/Breadcrumb';
 import RestartAltIcon from "@mui/icons-material/RestartAlt";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+
+import Breadcrumb from '../components/layout/Breadcrumb';
+import Loading from "../components/layout/Loading";
+import DoneIcon from "@mui/icons-material/Done";
+import ClearIcon from "@mui/icons-material/Clear";
+
+const forecastsTemp = [
+    {
+        "title": "Electricity produced by solar panels",
+        "description": "The amount of electricity produced by the solar panels, which are installed in the project",
+        "type": "float",
+        "unit": "[MWh per year]",
+        "minimum": "0",
+        "maximum": "-",
+        "id": "1"
+    },
+    {
+        "title": "Primary energy consumption after",
+        "description": "Primary energy consumption after installing the solar panel system",
+        "type": "float",
+        "unit": "[MWh per year]",
+        "minimum": "0",
+        "maximum": "-",
+        "id": "2"
+    },
+    {
+        "title": "Reduction of primary energy consumption",
+        "description": "Reduction of primary energy consumption: Difference between primary energy consumption before and after",
+        "type": "float",
+        "unit": "[MWh per year]",
+        "minimum": "0",
+        "maximum": "-",
+        "id": "3"
+    },
+    {
+        "title": "CO2 emissions reduction",
+        "description": "The amount of CO2 emissions reduction in the project",
+        "type": "float",
+        "unit": "[t CO2 per year]",
+        "minimum": "0",
+        "maximum": "-",
+        "id": "4"
+    }
+]
 
 const breadcrumbs = [
     <Link className={'breadcrumbLink'} key="1" to="/">
@@ -49,9 +91,6 @@ function PhotovoltaicInstallation() {
         Region: '',
     };
 
-    const [formData, setFormData] = useState(initialFormState);
-    const [accordionOpen, setAccordionOpen] = useState(true);
-
     const [formErrors, setFormErrors] = useState({
         ElectricityConsumption: false,
         PrimaryEnergyConsumptionBefore: false,
@@ -60,10 +99,15 @@ function PhotovoltaicInstallation() {
         Region: false,
     });
 
+    const [formData, setFormData] = useState(initialFormState);
+    const [accordionOpen, setAccordionOpen] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const [forecasts, setForecasts] = useState([]);
+
     const handleFormChange = (event) => {
-        const { name, value } = event.target;
-        setFormData({ ...formData, [name]: value });
-        setFormErrors({ ...formErrors, [name]: false });
+        const {name, value} = event.target;
+        setFormData({...formData, [name]: value});
+        setFormErrors({...formErrors, [name]: false});
     };
 
     const handleReset = () => {
@@ -94,15 +138,21 @@ function PhotovoltaicInstallation() {
         );
 
         if (isFormValid) {
+            setLoading(true);
+
             // Your save logic here
             console.log('Form saved:', formData);
+            setTimeout(() => {
+                setForecasts([...forecastsTemp]);
+                setLoading(false);
+            }, 2000);
         }
     };
 
     return (
         <>
-            <Breadcrumb breadcrumbs={breadcrumbs} welcome_msg={''} />
-            <Container maxWidth="xl" sx={{ my: 3 }}>
+            <Breadcrumb breadcrumbs={breadcrumbs} welcome_msg={''}/>
+            <Container maxWidth="xl" sx={{my: 3}}>
                 <Accordion
                     expanded={accordionOpen}
                     onChange={() => setAccordionOpen(!accordionOpen)}
@@ -113,34 +163,39 @@ function PhotovoltaicInstallation() {
                             justifyContent="space-between"
                             alignItems="center"
                         >
-                            <Grid item xs={12} sm={accordionOpen ? 6 : 2} style={{ textAlign: 'left' }}>
-                                <Typography variant="h6" component="div" style={{ flex: '2', wordWrap: 'break-word', color: '#9966ff', fontWeight: 'bold' }}>
+                            <Grid item xs={12} sm={accordionOpen ? 6 : 2} style={{textAlign: 'left'}}>
+                                <Typography variant="h6" component="div" style={{
+                                    flex: '2',
+                                    wordWrap: 'break-word',
+                                    color: '#9966ff',
+                                    fontWeight: 'bold'
+                                }}>
                                     Photovoltaic Installation Parameters
                                 </Typography>
                             </Grid>
                             {accordionOpen ? (
                                 <Button color="primary" onClick={() => setAccordionOpen(!accordionOpen)}>
-                                    <ExpandMoreIcon sx={{ color: '#9966ff' }} />
+                                    <ExpandMoreIcon sx={{color: '#9966ff'}}/>
                                 </Button>
                             ) : (
                                 <>
                                     {Object.entries(formData).map(([key, value]) => (
-                                        <Grid item xs={12} sm={1} key={key} style={{ textAlign: 'center' }}>
+                                        <Grid item xs={12} sm={1} key={key} style={{textAlign: 'center'}}>
                                             <Typography variant="body2">
                                                 <strong>{key.replace(/([A-Z])/g, ' $1').trim()}</strong>
-                                                <br /> {value || '-'}
+                                                <br/> {value || '-'}
                                             </Typography>
                                         </Grid>
                                     ))}
-                                    <Grid item xs={12} sm={2} style={{ textAlign: 'center' }}>
+                                    <Grid item xs={12} sm={2} style={{textAlign: 'center'}}>
                                         <Button
                                             color="primary"
                                             size={'large'}
                                             onClick={() => setAccordionOpen(!accordionOpen)}
                                             variant="outlined"
-                                            sx={{ borderColor: '#9966ff', color: '#9966ff' }}
+                                            sx={{borderColor: '#9966ff', color: '#9966ff'}}
                                         >
-                                            {accordionOpen ? <ExpandMoreIcon sx={{ color: '#9966ff' }} /> : 'EDIT'}
+                                            {accordionOpen ? <ExpandMoreIcon sx={{color: '#9966ff'}}/> : 'EDIT'}
                                         </Button>
                                     </Grid>
                                 </>
@@ -164,7 +219,7 @@ function PhotovoltaicInstallation() {
                                         'This field is required'
                                     }
                                     type="number"
-                                    inputProps={{ inputMode: 'numeric', min: 0 }}
+                                    inputProps={{inputMode: 'numeric', min: 0}}
                                 />
                             </Grid>
                             <Grid item xs={12} sm={4}>
@@ -181,7 +236,7 @@ function PhotovoltaicInstallation() {
                                         'This field is required'
                                     }
                                     type="number"
-                                    inputProps={{ inputMode: 'numeric', min: 0 }}
+                                    inputProps={{inputMode: 'numeric', min: 0}}
                                 />
                             </Grid>
                             <Grid item xs={12} sm={4}>
@@ -198,7 +253,7 @@ function PhotovoltaicInstallation() {
                                         'This field is required'
                                     }
                                     type="number"
-                                    inputProps={{ inputMode: 'numeric', min: 0 }}
+                                    inputProps={{inputMode: 'numeric', min: 0}}
                                 />
                             </Grid>
                             <Grid item xs={12} sm={4}>
@@ -215,7 +270,7 @@ function PhotovoltaicInstallation() {
                                         'This field is required'
                                     }
                                     type="number"
-                                    inputProps={{ inputMode: 'numeric', min: 0 }}
+                                    inputProps={{inputMode: 'numeric', min: 0}}
                                 />
                             </Grid>
                             <Grid item xs={12} sm={4}>
@@ -248,7 +303,7 @@ function PhotovoltaicInstallation() {
                                 variant="outlined"
                                 color="primary"
                                 onClick={handleReset}
-                                sx={{ borderColor: '#9966ff', color: '#9966ff', mx: 2 }}
+                                sx={{borderColor: '#9966ff', color: '#9966ff', mx: 2}}
                             >
                                 <RestartAltIcon/>
                                 RESET
@@ -269,6 +324,113 @@ function PhotovoltaicInstallation() {
                         </Grid>
                     </AccordionDetails>
                 </Accordion>
+
+                {loading && <Loading/>}
+
+                {!loading && forecasts.length > 0 && (
+                    <>
+                        <Typography
+                            variant={'h4'}
+                            sx={{
+                                color: theme.palette.primary.main,
+                                mt: 5,
+                                fontWeight: 'bold',
+                                borderBottom: '1px solid #9966ff',
+                            }}
+                        >
+                            Forecasts
+                        </Typography>
+                        <Grid container spacing={2} my={1}>
+                            {forecasts.map((forecast) => (
+                                <Grid
+                                    item
+                                    key={forecast.id}
+                                    xs={12}
+                                    md={3}
+                                >
+                                    <Card
+                                        sx={{
+                                            backgroundColor: 'white',
+                                            height: '100%',
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            justifyContent: 'space-between',
+                                        }}
+                                    >
+                                        <div
+                                            style={{
+                                                flex: 1,
+                                                display: 'flex',
+                                                flexDirection: 'column',
+                                            }}
+                                        >
+                                            <Typography
+                                                variant="h6"
+                                                align={'center'}
+                                                component="div"
+                                                fontWeight={'bold'}
+                                                sx={{
+                                                    marginTop: 0,
+                                                    alignSelf: 'flex-start',
+                                                    padding: '8px',
+                                                    mx: 'auto',
+                                                    color: theme.palette.primary.main,
+                                                }}
+                                            >
+                                                {forecast.title}
+                                            </Typography>
+                                            <div
+                                                style={{
+                                                    display: 'flex',
+                                                    flexDirection: 'column',
+                                                    alignItems: 'center',
+                                                    justifyContent: 'center',
+                                                }}
+                                            >
+                                                <img
+                                                    src={`/images/service2/id${forecast.id}.jpg`}
+                                                    alt={forecast.title}
+                                                    style={{
+                                                        marginTop: '20px',
+                                                        maxHeight: '140px',
+                                                        width: 'auto',
+                                                    }}
+                                                />
+                                                <Typography
+                                                    variant="body2"
+                                                    color="text.secondary"
+                                                    sx={{p: 2}}
+                                                >
+                                                    {forecast.description}
+                                                </Typography>
+                                            </div>
+                                        </div>
+                                        <Box
+                                            sx={{
+                                                display: 'flex',
+                                                justifyContent: 'center',
+                                                flexDirection: 'column',
+                                                marginBottom: '8px',
+                                            }}
+                                        >
+                                            <div
+                                                style={{
+                                                    display: 'flex',
+                                                    flexDirection: 'column',
+                                                    justifyContent: 'center',
+                                                    alignItems: 'center'
+                                                }}
+                                            >
+                                                <Typography variant={'h3'} fontWeight={'bold'} color={theme.palette.primary.main}>0.21</Typography>
+                                                <Typography variant={'body2'}>{forecast.unit}</Typography>
+                                            </div>
+                                        </Box>
+                                    </Card>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </>
+                )}
             </Container>
         </>
     );
